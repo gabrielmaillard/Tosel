0510f8f5381c1b3e02cc81f58aea5bbaf92cc011
Gabriel MAILLARD <maillard.gabriel2@gmail.com>
2024-04-16 19:13:42 +0200

    Nouveaux exercices et README

--- README.py
+++ README.py
@@ -5,20 +5,22 @@ def generate_readme():
     exercises_folder = "Exercices"
     readme_content = ""
 
-    exercise_files = []
+    exercises = {}
 
     for filename in os.listdir(exercises_folder):
         if filename.endswith(".JPG"):
-            exercise_files.append(filename)
+            exercise_number = filename.split(" ")[1].split(".")[0]
+            if exercise_number not in exercises:
+                exercises[exercise_number] = []
+            exercises[exercise_number].append(filename)
 
-    sorted_exercise_files = sorted(exercise_files, key=lambda x: int(x.split(" ")[1].split(".")[0]))
-
-    for filename in sorted_exercise_files:
-        exercise_number = filename.split(" ")[1].split(".")[0]
-        exercise_title = f"### Exercice {exercise_number}\n"
-        exercise_image_url = urllib.parse.quote(f"Exercices/{filename}")
-        exercise_image = f"![Exercice {exercise_number}]({exercise_image_url})\n\n"
-        readme_content += exercise_title + exercise_image
+    for exercise_number, filenames in sorted(exercises.items(), key=lambda x: int(x[0])):
+        exercise_title = f"\n### Exercice {exercise_number}\n"
+        exercise_images = ""
+        for filename in filenames:
+            exercise_image_url = urllib.parse.quote(f"Exercices/{filename}")
+            exercise_images += f"![Exercice {exercise_number}](Exercices/{exercise_image_url})\n"
+        readme_content += exercise_title + exercise_images
 
     with open("README.md", "w") as readme_file:
         readme_file.write(readme_content)
